9e1b0144cda341499fe66d9ebf0839d1b1d07635
martapalermo <77807333+martapalermo@users.noreply.github.com>
2021-06-02 09:21:32 -0700

    project before recycller view attempt

--- app/src/main/java/edu/neu/madcourse/madsu21_martapalermo/A4.java
+++ app/src/main/java/edu/neu/madcourse/madsu21_martapalermo/A4.java
@@ -1,8 +1,12 @@
 package edu.neu.madcourse.madsu21_martapalermo;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class A4 extends AppCompatActivity {
 
@@ -10,5 +14,16 @@ public class A4 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_linkcollector);
+
+        FloatingActionButton fab = findViewById(R.id.fabAdd);
+        fab.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Add url link here",
+                        Snackbar.LENGTH_LONG).setAction("Action", null).show();
+            }
+        });
     }
+
 }
